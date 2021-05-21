6b050dc714d27c89e9b7ebca5b67758fd79af506
Miklos Pavel <pavelmiklos1@gmail.com>
2021-05-21 09:41:01 +0200

    Initial Commit

---
+++ RafinaltBank/RafinaltBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafinaltBankApp.swift
+//  RafinaltBank
+//
+//  Created by Pável Miklós on 2021. 05. 21..
+//
+
+import SwiftUI
+
+@main
+struct RafinaltBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
